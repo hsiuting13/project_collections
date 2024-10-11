3601186f89e0aa3db18ec89fb83f003a10417654
Hsiu Ting <mr.hsiuting@gmail.com>
2024-10-11 01:02:33 -0700

    Add clock

---
+++ projects/js/clock.js
@@ -0,0 +1,26 @@
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+
+function clock(){
+  const date = new Date();
+  let seconds = date.getSeconds();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+//   console.log(date);
+//   console.log(seconds);
+//   console.log(minutes);
+//   console.log(hours);
+  seconds = (360 / 100) * ((seconds / 60) * 100);
+  minutes = (360 / 100) * ((minutes / 60) * 100);
+  hours = (360 / 100) * ((hours / 12) * 100);
+  let secondsAngle = seconds;
+  let minutesAngle = minutes;
+  let hoursAngle = hours;
+
+  hour.style.transform = "rotate(" + hoursAngle + "deg)";
+  minute.style.transform = "rotate(" + minutesAngle + "deg)";
+  second.style.transform = "rotate(" + secondsAngle + "deg)";
+}
+setInterval(clock, 1000)
+// clock();
